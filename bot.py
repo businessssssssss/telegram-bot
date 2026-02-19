7dca47f6b274c74b83db4b944a26412ed453a23e
Busines <business.chat12.02@gmail.com>
2026-02-19 10:09:30 +0200

    first commit

---
+++ bot.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.environ.get("TOKEN")
+MANAGER_ID = int(os.environ.get("MANAGER_ID"))
+
+app = Flask(__name__)
+application = ApplicationBuilder().token(TOKEN).build()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот працює 24/7 🚀")
+
+application.add_handler(CommandHandler("start", start))
+
+@app.route(f"/{TOKEN}", methods=["POST"])
+async def webhook():
+    update = Update.de_json(request.get_json(force=True), application.bot)
+    await application.process_update(update)
+    return "ok"
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+if __name__ == "__main__":
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        webhook_url=os.environ.get("RENDER_EXTERNAL_URL") + "/" + TOKEN
+    )
